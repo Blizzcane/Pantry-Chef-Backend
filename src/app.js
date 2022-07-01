e4c7d55a10d4e276a2f3da08ac8a8c05353c7641
Blizzcane <blizzcane@gmail.com>
2022-06-30 22:22:07 -0500

    setting up server

--- src/app.js
+++ src/app.js
@@ -3,11 +3,27 @@ const morgan = require("morgan");
 
 const app = express();
 
-const sayHello = (req, res, next) => {
-    res.send("Hello!");
+const sayHello = (req, res) => {
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : "Hello!";
+    res.send(content);
 };
+  
+
 
 app.use(morgan("dev"));
-app.use(sayHello);
+app.get("/hello", sayHello);
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.send(err);
+});
+  
+// Not-found handler
+app.use((req, res, next) => {
+    res.send(`The route ${req.path} does not exist!`);
+  });
 
 module.exports = app;
